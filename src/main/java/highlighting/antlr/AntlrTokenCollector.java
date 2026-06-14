7692f0926d85a41644ae64bb7eef83059305f121
Paul <paul.buesching@gmail.com>
2026-06-15 00:58:24 +0200

    Spotless Apply

--- src/main/java/highlighting/antlr/AntlrTokenCollector.java
+++ src/main/java/highlighting/antlr/AntlrTokenCollector.java
@@ -33,61 +33,87 @@ public class AntlrTokenCollector extends SyntaxHighlighter {
   // present).
   @Override
   public List<HighlightRegion> collectMatches(String text) {
-      MiniJavaLexer lexer = new MiniJavaLexer(CharStreams.fromString(text));
-      CommonTokenStream tokens = new CommonTokenStream(lexer);
-      List<HighlightRegion> regions = new ArrayList<>();
+    MiniJavaLexer lexer = new MiniJavaLexer(CharStreams.fromString(text));
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+    List<HighlightRegion> regions = new ArrayList<>();
 
-      for (Token token : tokens.getTokens()) {
-          //Skip EOF Token
-          if (token.getType() != Token.EOF) {
-              //Find Token Type
-              switch (token.getType()) {
-                  //Strings
-                  case MiniJavaLexer.STRING_LITERAL:
-                      regions.add(new HighlightRegion(token.getStartIndex(), token.getStopIndex() + 1, MiniJavaColours.STRING_LITERAL_COLOUR));
-                      break;
-                  //Chars
-                  case MiniJavaLexer.CHAR_LITERAL:
-                      regions.add(new HighlightRegion(token.getStartIndex(), token.getStopIndex() + 1, MiniJavaColours.CHAR_LITERAL_COLOUR));
-                      break;
-                  //Keywords
-                  case MiniJavaLexer.PACKAGE,
-                       MiniJavaLexer.IMPORT,
-                       MiniJavaLexer.CLASS,
-                       MiniJavaLexer.PUBLIC,
-                       MiniJavaLexer.PRIVATE,
-                       MiniJavaLexer.FINAL,
-                       MiniJavaLexer.RETURN,
-                       MiniJavaLexer.NULL,
-                       MiniJavaLexer.NEW,
-                       MiniJavaLexer.EXTENDS,
-                       MiniJavaLexer.IMPLEMENTS:
-                      regions.add(new HighlightRegion(token.getStartIndex(), token.getStopIndex() + 1, MiniJavaColours.KEYWORD_COLOUR));
-                      break;
-                  //Line Comments
-                  case MiniJavaLexer.LINE_COMMENT:
-                      regions.add(new HighlightRegion(token.getStartIndex(), token.getStopIndex() + 1, MiniJavaColours.LINE_COMMENT_COLOUR));
-                      break;
-                  //Block Kommentar
-                  case MiniJavaLexer.BLOCK_COMMENT:
-                      regions.add(new HighlightRegion(token.getStartIndex(), token.getStopIndex() + 1, MiniJavaColours.BLOCK_COMMENT_COLOUR));
-                      break;
-                  // Javadoc Kommentare
-                  case MiniJavaLexer.JAVADOC_COMMENT:
-                      regions.add(new HighlightRegion(token.getStartIndex(), token.getStopIndex() + 1, MiniJavaColours.JAVADOC_COMMENT_COLOUR));
-                      break;
-                  // Logik Strukturen
-                  case MiniJavaLexer.IF,
-                       MiniJavaLexer.ELSE,
-                       MiniJavaLexer.WHILE:
-                      regions.add(new HighlightRegion(token.getStartIndex(), token.getStopIndex() + 1, MiniJavaColours.STRING_LITERAL_COLOUR));
-                      break;
-                  // Andere werden nicht erkannt
-                  default:
-                      break;
-              }
-          }
+    for (Token token : tokens.getTokens()) {
+      // Skip EOF Token
+      if (token.getType() != Token.EOF) {
+        // Find Token Type
+        switch (token.getType()) {
+          // Strings
+          case MiniJavaLexer.STRING_LITERAL:
+            regions.add(
+                new HighlightRegion(
+                    token.getStartIndex(),
+                    token.getStopIndex() + 1,
+                    MiniJavaColours.STRING_LITERAL_COLOUR));
+            break;
+          // Chars
+          case MiniJavaLexer.CHAR_LITERAL:
+            regions.add(
+                new HighlightRegion(
+                    token.getStartIndex(),
+                    token.getStopIndex() + 1,
+                    MiniJavaColours.CHAR_LITERAL_COLOUR));
+            break;
+          // Keywords
+          case MiniJavaLexer.PACKAGE,
+          MiniJavaLexer.IMPORT,
+          MiniJavaLexer.CLASS,
+          MiniJavaLexer.PUBLIC,
+          MiniJavaLexer.PRIVATE,
+          MiniJavaLexer.FINAL,
+          MiniJavaLexer.RETURN,
+          MiniJavaLexer.NULL,
+          MiniJavaLexer.NEW,
+          MiniJavaLexer.EXTENDS,
+          MiniJavaLexer.IMPLEMENTS:
+            regions.add(
+                new HighlightRegion(
+                    token.getStartIndex(),
+                    token.getStopIndex() + 1,
+                    MiniJavaColours.KEYWORD_COLOUR));
+            break;
+          // Line Comments
+          case MiniJavaLexer.LINE_COMMENT:
+            regions.add(
+                new HighlightRegion(
+                    token.getStartIndex(),
+                    token.getStopIndex() + 1,
+                    MiniJavaColours.LINE_COMMENT_COLOUR));
+            break;
+          // Block Kommentar
+          case MiniJavaLexer.BLOCK_COMMENT:
+            regions.add(
+                new HighlightRegion(
+                    token.getStartIndex(),
+                    token.getStopIndex() + 1,
+                    MiniJavaColours.BLOCK_COMMENT_COLOUR));
+            break;
+          // Javadoc Kommentare
+          case MiniJavaLexer.JAVADOC_COMMENT:
+            regions.add(
+                new HighlightRegion(
+                    token.getStartIndex(),
+                    token.getStopIndex() + 1,
+                    MiniJavaColours.JAVADOC_COMMENT_COLOUR));
+            break;
+          // Logik Strukturen
+          case MiniJavaLexer.IF, MiniJavaLexer.ELSE, MiniJavaLexer.WHILE:
+            regions.add(
+                new HighlightRegion(
+                    token.getStartIndex(),
+                    token.getStopIndex() + 1,
+                    MiniJavaColours.STRING_LITERAL_COLOUR));
+            break;
+          // Andere werden nicht erkannt
+          default:
+            break;
+        }
       }
-      return regions;
+    }
+    return regions;
   }
 }
